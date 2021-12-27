fd8624c4a11b89712f31de2a3f2370983000e47f
James Huang <40145992+JamesHuangTC@users.noreply.github.com>
2021-12-28 02:53:07 +0800

    make it better

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def callback():
     return "OK"
 
 # 學你說話
-handler = WebhookHandler(parser)
+handler = WebhookHandler(channel_secret)
 @handler.add(MessageEvent, message=TextMessage)
 def echo(event):
     line_bot_api.reply_message(
