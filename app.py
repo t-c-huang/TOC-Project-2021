b21db1eb4c0ae5e144cca6aa027578a8e4c70944
James Huang <40145992+JamesHuangTC@users.noreply.github.com>
2021-12-28 22:59:43 +0800

    make it better

--- app.py
+++ app.py
@@ -75,7 +75,31 @@ def callback():
             continue
 
         line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(text=event.message.text)
+            event.reply_token,
+            #TextSendMessage(text=event.message.text)
+            TemplateSendMessage(
+                alt_text='Buttons template',
+                template=ButtonsTemplate(
+                    thumbnail_image_url='https://example.com/image.jpg',
+                    title='Menu',
+                    text='Please select',
+                    actions=[
+                        PostbackAction(
+                            label='postback',
+                            display_text='postback text',
+                            data='action=buy&itemid=1'
+                        ),
+                        MessageAction(
+                            label='message',
+                            text='message text'
+                        ),
+                        URIAction(
+                            label='uri',
+                            uri='http://example.com/'
+                        )
+                    ]
+                )
+            )
         )
 
     return "OK"
